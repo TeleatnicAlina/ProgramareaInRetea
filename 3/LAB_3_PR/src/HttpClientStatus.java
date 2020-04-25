6384bbc32a8b24761825be0a0868dadc0515e3ce
TeleatnicAlina <teleatnicalina@gmail.com>
2020-04-25 14:23:58 +0300

    Updated runner

--- 3/LAB_3_PR/src/HttpClientStatus.java
+++ 3/LAB_3_PR/src/HttpClientStatus.java
@@ -7,40 +7,29 @@ import java.net.http.HttpResponse;
 public class HttpClientStatus {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-
+        String ipAdddress = "51.158.172.165";
+        int port = 8811;//proxy port
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://unite.md/"))
                 .GET() // GET is default
                 .build();
-
         HttpResponse<Void> response = client.send(request,
                 HttpResponse.BodyHandlers.discarding());
 
-        System.out.println(response.statusCode());
-        System.out.println("GET request:");
+        if (response.statusCode() == 200) {
+            System.out.println("\n\n GET request:");
+            Get.sendGet(ipAdddress, port);
 
-        String ipAdddress = "51.158.172.165" ;
-        int port = 8811;//proxy port
+            System.out.println("\n\n HEAD request:");
+            Head.sendHead(ipAdddress, port);
 
-        Get.sendGet(ipAdddress, port);
+            System.out.println("\n\n OPTIONS request:");
+            Options.sendOptions(ipAdddress, port);
 
-        System.out.println(System.lineSeparator());
+            System.out.println("\n\n POST request:");
+            Post.sendPost(ipAdddress, port);
+        }
 
-        System.out.println("POST request:");
-
-        Post.sendPost(ipAdddress, port);
-
-        System.out.println(System.lineSeparator());
-
-        System.out.println("HEAD request:");
-
-        Head.sendHead(ipAdddress, port);
-
-        System.out.println(System.lineSeparator());
-
-        System.out.println("OPTIONS request:");
-
-        Options.sendOptions(ipAdddress, port);
     }
 }
